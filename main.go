95f8e431b8fd95c50546a51f5de85ac62afdf5af
kumasan <790118096@qq.com>
2021-06-02 11:00:30 +0800

    M reset main.go

--- main.go
+++ main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gohouse/gorose/v2"
-	config2 "kuma.com/kumashop/config"
 )
 
-var err error
-var engin *gorose.Engin
-
-func init() {
-	config, err := config2.ConfigFactory("mysql")
-	if err != nil {
-		panic(err)
-	}
-	engin, err = gorose.NewEngin(&gorose.Config{Driver: "mysql", Dsn: config.Url()})
-}
-
-func DB() gorose.IOrm {
-	return engin.NewOrm()
-}
-
 func main() {
-	db := DB()
-	res, err := db.Query("SELECT * FROM customer")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(res)
+
 }
